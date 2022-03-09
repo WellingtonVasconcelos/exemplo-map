8a1d13d534b0ecb69924c718270c5feb961ff293
Wellington Vasconcelos <wltvasc@hotmail.com>
2022-03-09 18:15:22 -0300

    Exemplo MAP com TreeMap/Get/Contains/Remove

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,6 +14,16 @@ public class Program {
 		cookies.put("phone", "996478044");
 		
 		cookies.remove("email");
+		/* O MAP é uma coleção de chave/ valor (K, V) que não adimite repetições do objeto chave no exemplo abaixo foi usado a mesma chave "phone" porém com o valor diferente
+		quando impresso o valor da chave abaixo subscreve o valor anterior*/
+		cookies.put("phone", "999832381");
+		
+		System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));//verificar se contem a chave
+		System.out.println("Phone number: " + cookies.get("phone"));//"get" pegar o valor de uma chave
+		System.out.println("Email: " + cookies.get("email"));//"get" pegar um valor inexistente o MAP retorna Nulo
+		
+		
+		
 		
 		System.out.println("ALL COOKIES: ");
 		for (String key : cookies.keySet()) {
